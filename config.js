90f726f087ec676a5b26a08e59c469137447af78
panthercast304-eng <panthercast304@gmail.com>
2026-09-18 08:03:26 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Only public client configuration belongs here.
-// NEVER put a Supabase secret/service-role key in GitHub.
-window.SUPABASE_URL="https://gpqhsclilrzlxrxlrjxv.supabase.co";
-window.SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcWhzY2xpbHJ6bHhyeGxyanh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3OTM5OTksImV4cCI6MjEwNDM2OTk5OX0.Nj9VKVwpLCvWh_EkHtNK0X7W1UHVGRH2tIo1mEm47II";
